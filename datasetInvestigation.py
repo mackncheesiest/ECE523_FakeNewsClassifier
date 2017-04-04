85493daba90a7db09d7b8de778312cf30bb43771
mackncheesiest <mackncheesiest@gmail.com>
2017-04-03 23:05:15 -0700

    datasetInvestigation now results in a ['TITLE', 'FAKE'] dataset
    that can be used for classification on the basis of article title
    alone

--- datasetInvestigation.py
+++ datasetInvestigation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # I don't even know if I can find all the stack overflow posts I used to help me figure out all this data wrangling
 # Pretend I linked them here
@@ -7,7 +8,6 @@ fakenews = pd.read_csv('./datasets/fake.csv')
 ucinews = pd.read_csv('./datasets/uci-news-aggregator.csv')
 
 # We want to do the intersection of the hostnames in the fake news
-
 # First, rename the site_url of fakenews to HOSTNAME so that the column matches with ucinews
 fakenews = fakenews.rename(columns={'site_url': 'HOSTNAME', 'title': 'TITLE'})
 
@@ -19,16 +19,20 @@ fakenews_subset = fakenews.drop_duplicates(subset=['HOSTNAME'])
 ucinews_subset = ucinews.drop_duplicates(subset=['HOSTNAME'])
 
 # Finally, now that the data is nice and comparable, compare the two to see if there are any sites in common
-# Build a full intersectionSet so that we can see how much data we have in common between the two
-intersectionSet = pd.merge(fakenews, ucinews, how='inner', on=['HOSTNAME'])
-# And also, intersect the subset sets so that we can see which hostnames appear in each
 intersectionSet_subset = pd.merge(fakenews_subset, ucinews_subset, how='inner', on=['HOSTNAME'])
 
 # And now that we're confident that there are at least sites in common between the two, build one dataset that contains information from both datasets
-# For now, let's just do it based on TITLE and HOSTNAME
+# For now, let's just do it based on TITLE and Fake/Not Fake
+print "There are %d hostnames in common between the two datasets" % (intersectionSet_subset.shape[0])
 
-# TODO: Figure out what to include in here for classification labels. 
-# Also, we need to merge the type of story from fake news ("bias", "fake", ...) with the labels from uci ("e" = entertainment, "s" = science, ...)
-conjoinedDataset = pd.DataFrame({'HOSTNAME': fakenews['HOSTNAME'], 'TITLE': fakenews['TITLE']}).append(
-        pd.DataFrame({'HOSTNAME': ucinews['HOSTNAME'], 'TITLE': ucinews['TITLE']})
-        )
+# Add a "FAKE" column to uci dataset and initialize it to zeros
+ucinews['FAKE'] = np.zeros([ucinews.shape[0]])
+# But fill it with 1's for websites that are contained within the fake news dataset
+ucinews.loc[ucinews['HOSTNAME'].isin(intersectionSet_subset['HOSTNAME']), 'FAKE'] = 1
+
+# Then, shrink each of the two DataFrames to contain only the "TITLE" and "FAKE" datapoints
+fakenews = pd.DataFrame({'TITLE': fakenews['TITLE'], 'FAKE': np.ones([fakenews.shape[0]])})
+ucinews = pd.DataFrame({'TITLE': ucinews['TITLE'], 'FAKE': ucinews['FAKE']})
+
+# Conjoin them into one DataFrame, and we have ourselves a dataset to work with
+conjoinedDataset = ucinews.append(fakenews)
