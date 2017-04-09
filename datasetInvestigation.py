c24182e510d6b89c3d8b0e9effd015dd426dc415
mackncheesiest <mackncheesiest@gmail.com>
2017-04-09 15:05:04 -0700

    Got first example working with provided llda library
    Need to do more preprocessing to remove words that don't distinguish
    topics like "a", "the", "but", etc.
    
    Also, I'm not really sure how "testing" works with this model.
    It's not like we have a classifier.score() like many sklearn options do

--- datasetInvestigation.py
+++ datasetInvestigation.py
@@ -3,36 +3,45 @@ import numpy as np
 
 # I don't even know if I can find all the stack overflow posts I used to help me figure out all this data wrangling
 # Pretend I linked them here
-
-fakenews = pd.read_csv('./datasets/fake.csv')
-ucinews = pd.read_csv('./datasets/uci-news-aggregator.csv')
-
-# We want to do the intersection of the hostnames in the fake news
-# First, rename the site_url of fakenews to HOSTNAME so that the column matches with ucinews
-fakenews = fakenews.rename(columns={'site_url': 'HOSTNAME', 'title': 'TITLE'})
-
-# Then, remove www. from the front of all UCI hostnames that don't have them (none of the fakenews sites have them and we want to compare them)
-ucinews.loc[ucinews['HOSTNAME'].str[0:4] == 'www.', 'HOSTNAME'] = ucinews.loc[ucinews['HOSTNAME'].str[0:4] == 'www.', 'HOSTNAME'].str[4:]
-
-# Essentially just build a list of all the hostnames within each dataset so that we can check if we have any in common
-fakenews_subset = fakenews.drop_duplicates(subset=['HOSTNAME'])
-ucinews_subset = ucinews.drop_duplicates(subset=['HOSTNAME'])
-
-# Finally, now that the data is nice and comparable, compare the two to see if there are any sites in common
-intersectionSet_subset = pd.merge(fakenews_subset, ucinews_subset, how='inner', on=['HOSTNAME'])
-
-# And now that we're confident that there are at least sites in common between the two, build one dataset that contains information from both datasets
-# For now, let's just do it based on TITLE and Fake/Not Fake
-print "There are %d hostnames in common between the two datasets" % (intersectionSet_subset.shape[0])
-
-# Add a "FAKE" column to uci dataset and initialize it to zeros
-ucinews['FAKE'] = np.zeros([ucinews.shape[0]])
-# But fill it with 1's for websites that are contained within the fake news dataset
-ucinews.loc[ucinews['HOSTNAME'].isin(intersectionSet_subset['HOSTNAME']), 'FAKE'] = 1
-
-# Then, shrink each of the two DataFrames to contain only the "TITLE" and "FAKE" datapoints
-fakenews = pd.DataFrame({'TITLE': fakenews['TITLE'], 'FAKE': np.ones([fakenews.shape[0]])})
-ucinews = pd.DataFrame({'TITLE': ucinews['TITLE'], 'FAKE': ucinews['FAKE']})
-
-# Conjoin them into one DataFrame, and we have ourselves a dataset to work with
-conjoinedDataset = ucinews.append(fakenews)
+def extractDataset():
+    fakenews = pd.read_csv('./datasets/fake.csv')
+    ucinews = pd.read_csv('./datasets/uci-news-aggregator.csv')
+    
+    # We want to do the intersection of the hostnames in the fake news
+    # First, rename the site_url of fakenews to HOSTNAME so that the column matches with ucinews
+    fakenews = fakenews.rename(columns={'site_url': 'HOSTNAME', 'title': 'TITLE'})
+    
+    # Then, remove www. from the front of all UCI hostnames that don't have them (none of the fakenews sites have them and we want to compare them)
+    ucinews.loc[ucinews['HOSTNAME'].str[0:4] == 'www.', 'HOSTNAME'] = ucinews.loc[ucinews['HOSTNAME'].str[0:4] == 'www.', 'HOSTNAME'].str[4:]
+    
+    # Essentially just build a list of all the hostnames within each dataset so that we can check if we have any in common
+    fakenews_subset = fakenews.drop_duplicates(subset=['HOSTNAME'])
+    ucinews_subset = ucinews.drop_duplicates(subset=['HOSTNAME'])
+    
+    # Finally, now that the data is nice and comparable, compare the two to see if there are any sites in common
+    intersectionSet_subset = pd.merge(fakenews_subset, ucinews_subset, how='inner', on=['HOSTNAME'])
+    
+    # And now that we're confident that there are at least sites in common between the two, build one dataset that contains information from both datasets
+    # For now, let's just do it based on TITLE and Fake/Not Fake
+    print "There are %d hostnames in common between the two datasets" % (intersectionSet_subset.shape[0])
+    
+    # Add a "FAKE" column to uci dataset and initialize it to zeros
+    ucinews['FAKE'] = np.zeros([ucinews.shape[0]])
+    # But fill it with 1's for websites that are contained within the fake news dataset
+    ucinews.loc[ucinews['HOSTNAME'].isin(intersectionSet_subset['HOSTNAME']), 'FAKE'] = 1
+    
+    # Then, shrink each of the two DataFrames to contain only the "TITLE" and "FAKE" datapoints
+    fakenews = np.array(pd.DataFrame({'TITLE': fakenews['TITLE'], 'FAKE': np.ones([fakenews.shape[0]])}))
+    ucinews = np.array(pd.DataFrame({'TITLE': ucinews['TITLE'], 'FAKE': ucinews['FAKE']}))
+    
+    
+    # Conjoin them into one DataFrame, and we have ourselves a dataset to work with
+    conjoinedDataset = np.concatenate((ucinews, fakenews))
+    conjoinedDataset = pd.DataFrame({'TITLE': conjoinedDataset[:, 1], 'FAKE': conjoinedDataset[:, 0]})
+    
+    # Some of the titles are turned into "NaN"s, so drop them
+    conjoinedDataset = conjoinedDataset.dropna()
+    
+    #conjoinedDataset['FAKE'] = map(str, conjoinedDataset['FAKE'])
+    
+    return conjoinedDataset
---
+++ fakeNews_LLDA.py
@@ -0,0 +1,62 @@
+# Import the LLDA (labeled latent dirichlet allocation) class that we're so interested in
+from llda import LLDA
+# Import our code for combining the "real" and "fake" datsets into something usable
+from datasetInvestigation import extractDataset
+# Numpy is always handy
+import numpy as np
+# This lets us split the data into training and testing sets
+from sklearn.model_selection import train_test_split
+# This lets us call map with str.split (thanks http://stackoverflow.com/questions/8461230/python-map-string-split-list)
+from operator import methodcaller
+# And the dev we're borrowing from  prints to sys.stderr in the nltk example, so for now lets just keep doing that
+import sys
+
+# Grab the combined dataset and turn it into a numpy array I guess
+totalDataset = np.array(extractDataset());
+# Put the class labels on the last column because that's how train_test_split likes it
+totalDataset[:, 0], totalDataset[:, 1] = totalDataset[:, 1], totalDataset[:, 0].copy()
+
+trainSet, testSet = train_test_split(totalDataset, test_size=0.9)
+
+# Turn this dataset into a list of strings
+# Then, split each string into its own list because that's what llda.py wants
+corpus = map(methodcaller("split", " "), list(trainSet[:, 0]))
+
+# Turn the 1.0/0.0 float labels into 1 or 0
+# Then turn them into strings
+# Then turn each string into a one entry "list" of strings
+# Because that's what this llda.py wants I guess
+labels = map(list, map(str, map(int, trainSet[:, 1])))
+
+# The labels that we're interested in are '0' for real and '1' for fake
+labelset = ['0', '1']
+
+# I don't know what these parameters are, but lets keep them how they are in the llda_nltk example
+K = 20
+alpha = 0.001
+beta = 0.001
+iteration = 100
+
+# Instantiate an LLDA object and "set the corpus" which is probably building some kind of internal model
+llda = LLDA(K, alpha, beta)
+llda.set_corpus(labelset, corpus, labels)
+
+# Then, do iteration-many instances of "inference" -- again, whatever that means. Hopefully it's, like, predicting?
+# TODO: Figure out what exactly is happening here
+
+# Actually, I think this is basically like training. Ideally, lower perplexity in each step is good going off of Wikipedia
+# Perplexity is a measure of how well a probability distribution predicts a sample, with lower being better
+for i in range(iteration):
+    sys.stderr.write("-- %d : %.4f\n" % (i, llda.perplexity()))
+    llda.inference()
+print "perplexity : %.4f" % llda.perplexity()
+
+# And I have no idea what this does right now
+# TODO: Figure out what this does
+
+# Probably printing out some kind of statistics about the trained model
+phi = llda.phi()
+for k, label in enumerate(labelset):
+    print "\n-- label %d : %s" % (k, label)
+    for w in np.argsort(-phi[k])[:20]:
+        print "%s: %.4f" % (llda.vocas[w], phi[k,w])
