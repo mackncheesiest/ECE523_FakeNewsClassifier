9c69b679b06434d2e7d21526d6b31cc923e8eb5f
mackncheesiest <mackncheesiest@gmail.com>
2017-04-23 11:50:15 -0700

    Added dot-product method of classification/testing

--- fakeNews_LLDA.py
+++ fakeNews_LLDA.py
@@ -6,8 +6,6 @@ from datasetInvestigation import extractDataset
 import numpy as np
 # This lets us split the data into training and testing sets
 from sklearn.model_selection import train_test_split
-# This lets us call map with str.split (thanks http://stackoverflow.com/questions/8461230/python-map-string-split-list)
-from operator import methodcaller
 # And the dev we're borrowing from  prints to sys.stderr in the nltk example, so for now lets just keep doing that
 import sys
 
@@ -16,7 +14,7 @@ totalDataset = np.array(extractDataset());
 # Put the class labels on the last column because that's how train_test_split likes it
 totalDataset[:, 0], totalDataset[:, 1] = totalDataset[:, 1], totalDataset[:, 0].copy()
 
-trainSet, testSet = train_test_split(totalDataset, test_size=0.8)
+trainSet, testSet = train_test_split(totalDataset, test_size=0.2)
 
 # Turn this dataset into a list of strings
 # Then, split each string into its own list because that's what llda.py wants
@@ -43,40 +41,83 @@ labels = map(list, map(str, map(int, trainSet[:, 1])))
 # The labels that we're interested in are '0' for real and '1' for fake
 labelset = ['0', '1']
 
-# I don't know what these parameters are, but lets keep them how they are in the llda_nltk example
-# Oh, K is the number of topics, so we want that to be 2, not the default of 20
+# K is the number of topics, so we want that to be 2, not the default of 20
 K = 2
 # Alpha and Beta are something to do with controlling similarity between topics and words within a topic
 # This video explained it and I forgot https://www.youtube.com/watch?v=3mHy4OSyRf0
+# Lets keep them how they are in the llda_nltk example
 alpha = 0.0001
 beta = 0.0001
-iterations = 50
+# So, this seems to converge to a top 20 list *really* fast, so lets do like 10 iterations
+iterations = 10
 
 # Instantiate an LLDA object and "set the corpus" which is probably building some kind of internal model
 llda = LLDA(K, alpha, beta)
 llda.set_corpus(labelset, corpus, labels)
 
-# Then, do iteration-many instances of "inference" -- again, whatever that means. Hopefully it's, like, predicting?
-# TODO: Figure out what exactly is happening here
-
-# Actually, I think this is basically like training. Ideally, lower perplexity in each step is good going off of Wikipedia
+# Then, do some kind of training prodecure thing. Inference is better if it's lower or something.
+# Ideally, lower perplexity in each step is good going off of Wikipedia
 # Perplexity is a measure of how well a probability distribution predicts a sample, with lower being better
 for i in range(iterations):
     sys.stderr.write("-- %d : %.4f\n" % (i, llda.perplexity()))
     llda.inference()
     
-    phi = llda.phi()
-    for k, label in enumerate(labelset):
-        print "\n-- label %d : %s" % (k, label)
-        for w in np.argsort(-phi[k])[:20]:
-            print "%s: %.4f" % (llda.vocas[w], phi[k,w])
+    #phi = llda.phi()
+    #for k, label in enumerate(labelset):
+    #    print "\n-- label %d : %s" % (k, label)
+    #    for w in np.argsort(-phi[k])[:20]:
+    #        print "%s: %.4f" % (llda.vocas[w], phi[k,w])
 print "perplexity : %.4f" % llda.perplexity()
 
-# And I have no idea what this does right now
-# TODO: Figure out what this does
-# I think it's just printing out the 20 most common words in each topic
+# This prints out the 40 most likely words in each topic
+#phi = llda.phi()
+#for k, label in enumerate(labelset):
+#    print "\n-- label %d : %s" % (k, label)
+#    for w in np.argsort(-phi[k])[:40]:
+#        print "%s: %.4f" % (llda.vocas[w], phi[k,w])
+        
+testCorpus = list(testSet[:, 0])
+
+trimmedTestCorpus = []
+for i in range(len(testCorpus)):
+    temp = [word for word in testCorpus[i].split() if word.lower() not in stopwords]
+    trimmedTestCorpus.append(temp)
+    
 phi = llda.phi()
-for k, label in enumerate(labelset):
-    print "\n-- label %d : %s" % (k, label)
-    for w in np.argsort(-phi[k])[:20]:
-        print "%s: %.4f" % (llda.vocas[w], phi[k,w])
+totalPredictionsCorrect = 0
+for i in range(len(testSet[:, 0])):
+#for i in range(1000):
+    # Each column is a one-hot entry indicating the existence of a particular word from the vocab in the dataset
+    oneHotVector = np.zeros([1, len(llda.vocas)])
+    for j in range(len(trimmedTestCorpus[i][:])):
+        if trimmedTestCorpus[i][j] in llda.vocas:
+            oneHotVector[0, llda.vocas.index(trimmedTestCorpus[i][j])] = 1
+    # Then, dot product that one-hot vector with the phi vector and see what the most likely topic is
+    #print("Vector under consideration: " + str(trimmedTestCorpus[i]))
+    
+    #dotProductCommon = np.dot(oneHotVector[0, :], phi[0, :])
+    dotProductReal = np.dot(oneHotVector[0, :], phi[1, :])
+    dotProductFake = np.dot(oneHotVector[0, :], phi[2, :])
+    
+    #print("dot product with topic common: " + str(dotProductCommon))
+    #print("dot product with topic real: " + str(dotProductReal))
+    #print("dot product with topic fake: " + str(dotProductFake))
+    
+    if dotProductReal >= dotProductFake:
+        #print("Classified as real")
+        expected = 0.0
+    else:
+        #print("Classified as fake")
+        expected = 1.0
+        
+    #print("Actual label: " + str(testSet[i, 1]))
+    
+    if expected == testSet[i, 1]:
+        totalPredictionsCorrect = totalPredictionsCorrect + 1
+    
+    if i % 10000 == 0 and i > 0:
+        print("i = " + str(i) + ", " + str(100.*i/len(testSet[:, 0])) + "% done")
+    
+    #print("\n\n")
+
+print("Testing Accuracy: " + str((100.*totalPredictionsCorrect)/len(testSet[:, 0])))
