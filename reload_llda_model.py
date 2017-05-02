98cbdd265214c4f2ac287435bf079257533a0582
mackncheesiest <mackncheesiest@gmail.com>
2017-05-02 08:42:44 -0700

    Added computation of recall and precision as well, #1

--- reload_llda_model.py
+++ reload_llda_model.py
@@ -33,6 +33,12 @@ def test_llda_model(llda, testSet):
     pred_classes = np.zeros([np.size(testSet, axis=0)])
     true_classes = np.float64(np.reshape(testSet[:, 1], [np.size(testSet, axis=0)]))
         
+    # True positives (predicted fake and is fake), false positives (pred fake and is real), 
+    # and false negatives (pred real and is fake), and fal
+    TP = 0
+    FP = 0
+    FN = 0
+    
     for i in range(len(testSet[:, 0])):
     #for i in range(1000):
         # Each column is a one-hot entry indicating the existence of a particular word from the vocab in the dataset
@@ -53,9 +59,20 @@ def test_llda_model(llda, testSet):
         pred_classes[i] = expected        
         if expected == testSet[i, 1]:
             totalPredictionsCorrect = totalPredictionsCorrect + 1
+            
+        if expected == 1.0 and testSet[i, 1] == 1.0:
+            TP += 1
+        if expected == 1.0 and testSet[i, 1] == 0.0:
+            FP += 1
+        if expected == 0.0 and testSet[i, 1] == 1.0:
+            FN += 1
         
         if i % 10000 == 0 and i > 0:
             print("i = " + str(i) + ", " + str(100.*i/len(testSet[:, 0])) + "% done")
         
     print("Testing Accuracy: " + str((100.*totalPredictionsCorrect)/len(testSet[:, 0])))
     print("Matthews Correlation Coeff: " + str(matthews_corrcoef(y_true=true_classes, y_pred=pred_classes)))
+    #Recall = True Positives / (True Positives + False Negatives)
+    print("Recall: " + str(100.*TP/(TP + FN)))
+    #Precision = True Positives / (True Positives + False Positives)
+    print("Precision: " + str(100.*TP/(TP + FP)))
